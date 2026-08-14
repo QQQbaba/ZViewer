a420494dc0ac172e201ac8906447ff15363433fe
Zero <Zero_wyc@qq.com>
2026-08-15 01:22:18 +0800

    删除不必要文件

--- _tmp_make_token.js
+++
@@ -1,3 +0,0 @@
-const jwt = require('./node_modules/jsonwebtoken');
-const token = jwt.sign({ userId: 1, role: 'root', username: 'root' }, 'dev-access-secret-change-in-production', { expiresIn: '10m' });
-console.log(token);
